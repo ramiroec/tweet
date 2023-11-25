ffe1e6c41881fa08136a1bcc5bb0ea8ea46954e6
ramiroec <ramiroec@hotmail.com>
2023-11-25 14:01:26 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+//Importar React-tweet
+import {Tweet} from 'react-tweet'
+
+export default function App() {
+  return (
+    <main>
+      <Tweet id='1683749887551197189' />
+    </main>
+  )
+}
